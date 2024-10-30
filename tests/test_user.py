99704f20558f38ddca6ee20c50f7c2d6b96c9dee
izik <bwanfomisaac@gmail.com>
2024-10-30 01:53:45 +0100

    delete some tests in test_user.py (will be right back)

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,12 +4,9 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 import pytest
+import uuid
 from app.db.database import init_db
 from app.docker.docker_utils import start_cassandra_container, stop_cassandra_container
-import uuid
-from app.db.models import User
-from uuid import uuid4
-from datetime import timezone, datetime
 from httpx import AsyncClient
 from app.main import app
 
@@ -58,64 +55,6 @@ def generate_unique_wallet_address():
 #     assert response.status_code == 200
 #     assert response.json()["display_name"] == unique_display_name
 
-@pytest.mark.asyncio
-async def test_get_admin_users(setup_admin_users):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.get("/users/admins")
-    
-    # Check response status
-    assert response.status_code == 200, f"Unexpected status code {response.status_code}, Response: {response.json()}"
-
-    # Parse response JSON data
-    data = response.json()
-    
-    # Check the number of returned admins matches the setup
-    assert len(data) == 2, f"Expected 2 admins, got {len(data)}. Response: {data}"
-
-    # Validate data structure for each admin user
-    for user_data in data:
-        assert "id" in user_data, f"Missing 'id' in user_data: {user_data}"
-        assert "wallet_address" in user_data, f"Missing 'wallet_address' in user_data: {user_data}"
-        assert "display_name" in user_data, f"Missing 'display_name' in user_data: {user_data}"
-        assert "bio" in user_data, f"Missing 'bio' in user_data: {user_data}"
-        assert "profile_photo_url" in user_data, f"Missing 'profile_photo_url' in user_data: {user_data}"
-        assert "created_at" in user_data, f"Missing 'created_at' in user_data: {user_data}"
-        assert "tags" in user_data, f"Missing 'tags' in user_data: {user_data}"
-        assert "admin" in user_data["tags"], f"Expected 'admin' tag in 'tags': {user_data['tags']}"
-
-
-@pytest.fixture(scope="function")
-def setup_admin_users():
-    # Retrieve all users with the "admin" tag for cleanup
-    admin_users = User.objects.filter(tags__contains=["admin"]).all()
-    for admin_user in admin_users:
-        admin_user.delete()
-
-    # Create valid admin users for testing
-    admin_user_1 = User.create(
-        id=uuid4(),
-        wallet_address="TcsCu4yjc2GFZCXPVkxQ6E54MWCHkdT9z2",  # random wallet addy for testing
-        display_name="admin_user_1",
-        bio="Admin User 1 Bio",
-        profile_photo_url="/lascaux-backend/img/coolguy.jpg",
-        created_at=datetime.now(timezone.utc),
-        tags=["admin"]
-    )
-    admin_user_2 = User.create(
-        id=uuid4(),
-        wallet_address="TvdqDw3ZzLrYwr3qukMj86rf3dtMQwL5PU",  # random wallet addy for testing
-        display_name="admin_user_2",
-        bio="Admin User 2 Bio",
-        profile_photo_url="/lascaux-backend/img/angrycat.jpg",
-        created_at=datetime.now(timezone.utc),
-        tags=["admin"]
-    )
-
-    yield [admin_user_1, admin_user_2]
-
-    # Clean up after the test by deleting the specific users created
-    admin_user_1.delete()
-    admin_user_2.delete()
 
     
 
