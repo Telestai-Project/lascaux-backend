0f5e7ab99270db7afc2858c4c457a6c0731b0b0b
Zach Price <zach.price105@gmail.com>
2024-12-24 11:44:32 -0700

    couple of minor changes

--- app/utils/discord_utils.py
+++ app/utils/discord_utils.py
@@ -28,7 +28,7 @@ default_profile = ExecutionProfile(
 
 cluster = Cluster(
     ['127.0.0.1'],
-    port=9042,
+    port=9052,
     connect_timeout=10.0,
     execution_profiles={
         EXEC_PROFILE_DEFAULT: default_profile
